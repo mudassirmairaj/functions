3436ba9aaf68a7f63004027fac09cffb367ff2eb
MufakirMairaj <mufakirmairaj5@gmail.com>
2021-05-16 10:28:19 +0500

    average of three

---
+++ average.js
@@ -0,0 +1,8 @@
+let average = function (num1, num2, num3) {
+  let total = (num1 + num2 + num3) / 3;
+  return total;
+};
+
+console.log(average(3, 10, 8)); // 7
+console.log(average(10, 5, 12)); // 9
+console.log(average(6, 20, 40)); // 22
